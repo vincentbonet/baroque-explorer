6a353442323caa193ab932aa36280f9c79572500
vincentbonet <141579416+vincentbonet@users.noreply.github.com>
2026-05-23 19:35:14 -0400

    feat: updated .gitignore used node:sqlite3 as previous sql packages were giving compatibility troubles

--- server/database.js
+++ server/database.js
@@ -1,12 +1,13 @@
-import Database from 'better-sqlite3';
+import { DatabaseSync } from 'node:sqlite';
 import { readFileSync } from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-export const db = new Database(join(__dirname, 'baroque.db'));
-db.pragma('journal_mode = WAL');
+export const db = new DatabaseSync(join(__dirname, 'baroque.db'));
+
+db.exec('PRAGMA journal_mode = WAL');
 
 const seed = readFileSync(join(__dirname, 'baroque-seed.sql'), 'utf8');
 db.exec(seed);
